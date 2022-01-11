4adf8c44c8fff64d3e2659c5e1c7f90c0c43d0e1
MSH <sehyun37@naver.com>
2022-01-11 16:57:35 +0900

    추가 작성

--- MyJava/src/test/main/MainClass01.java
+++ MyJava/src/test/main/MainClass01.java
@@ -1,5 +1,7 @@
 package test.main;
 
 public class MainClass01 {
-
+	public static void mian(String[] args) {
+		//작성중..
+	}
 }
---
+++ MyJava/src/test/main/MyClass.java
@@ -0,0 +1,8 @@
+package test.main;
+
+public class MyClass {
+	public static void main(String[] args) {
+		System.out.println("무언가 작성중!");
+	}
+	
+}
